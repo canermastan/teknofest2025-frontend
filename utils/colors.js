96a6be6a3400622ae1ec5a6a53a6bb89b5818640
canermastan <jcanermastan@gmail.com>
2024-12-18 21:14:39 +0300

    feature: change primary color

--- utils/colors.js
+++ utils/colors.js
@@ -1,7 +1,8 @@
 export const colors = {
   white: '#FFFFFF',
-  // primary: "#45484A",
-  primary: '#1E90FF',
+  // primary: "#45484A", // gray  
+  // primary: '#1E90FF', // blue
+  primary: '#F22A26', // between red20 and red30
   secondary: '#AEB5BB',
   gray: '#D9D9D9',
   black:'#4F4F4F',
